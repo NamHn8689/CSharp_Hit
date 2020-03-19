d65e1337f02875fdcd1761da30a8dc1320f778a4
namkeke <phuongnamhn2000@gmail.com>
2020-03-19 22:24:54 +0700

    Day 2

---
+++ Day2/Ex1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    class Day2
+    {
+        static void Main(string[] args)
+        {
+            int n = input();
+            int[] a = new int[n];
+            inputArray(a, n);
+            show(a, n);
+            Console.ReadLine();
+        }
+        static int input()
+        {
+            int n;
+            do
+            {
+                Console.Write("Input n(n>0): ");
+                n = int.Parse(Console.ReadLine());
+            } while (n <= 0);
+            return n;
+        }
+        static void inputArray(int[] a, int n)
+        {
+            Console.WriteLine("Input array: ");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("a[" + i + "] = ");
+                a[i] = int.Parse(Console.ReadLine());
+            }
+        }
+        static bool isPrime(int x)
+        {
+            if (x < 2)
+                return false;
+            for (int i = 2; i * i <= x; i++)
+                if (x % i == 0)
+                    return false;
+            return true;
+        }
+        static void show(int[] a, int n)
+        {
+            Console.Write("Is prime: ");
+            for (int i = 0; i < n; i++)
+                if (isPrime(i+1))
+                    Console.Write(a[i] + " ");
+        }
+    }
+}
---
+++ Day2/Ex4.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace CSharp
+{
+    class Day2
+    {
+        struct Date
+        {
+            public int day, month, year;
+        }
+        struct Student
+        {
+            public String id, name;
+            public Date dateOfBirth;
+        }
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("Inut number of students: ");
+            n = int.Parse(Console.ReadLine());
+            Student[] listStudent = new Student[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Enter the information of student " + (i + 1)+":");
+                listStudent[i] = inputInfor();
+            }
+            Console.Clear();
+            Console.WriteLine("Student list:");
+            showList(listStudent);
+            sortByYear(listStudent);
+            Console.WriteLine("Studen list after sort:");
+            showList(listStudent);
+            Console.ReadLine();
+        }
+        static Student inputInfor()
+        {
+            Student x;
+            Console.Write("Input id: ");
+            x.id = Console.ReadLine();
+            Console.Write("Input name: ");
+            x.name = Console.ReadLine();
+            Console.WriteLine("Input date of birth");
+            Console.Write("Input day: ");
+            x.dateOfBirth.day = int.Parse(Console.ReadLine());
+            Console.Write("Input month: ");
+            x.dateOfBirth.month = int.Parse(Console.ReadLine());
+            Console.Write("Input year: ");
+            x.dateOfBirth.year = int.Parse(Console.ReadLine());
+            return x;
+        }
+        static void showInfor(Student x)
+        {
+            Console.WriteLine("Id: " + x.id + "\nname: " + x.name + "\nDate of birth: " + x.dateOfBirth.day + "/" + x.dateOfBirth.month + "/" + x.dateOfBirth.year);
+        }
+        static void showList(Student[] listStudent)
+        {
+            Console.WriteLine();
+            int stt = 1;
+            foreach (Student x in listStudent)
+            {
+                Console.WriteLine(stt + ".");
+                showInfor(x);
+                stt++;
+            }
+            Console.WriteLine();
+        }
+        static void sortByYear(Student[] listStudent)
+        {
+            for (int i = 0; i < listStudent.Length - 1; i++)
+            {
+                for (int j = i + 1; j < listStudent.Length; j++)
+                {
+                    if (listStudent[i].dateOfBirth.year > listStudent[j].dateOfBirth.year)
+                    {
+                        Student temp = listStudent[i];
+                        listStudent[i] = listStudent[j];
+                        listStudent[j] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
