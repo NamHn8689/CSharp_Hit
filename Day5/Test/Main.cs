9a8e81e38ca4c03e3cedd1a8e11ed95b77acf1a5
namkeke <phuongnamhn2000@gmail.com>
2020-05-17 20:07:15 +0700

    Day 5

---
+++ Day5/Test/Main.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day5
+{
+    class main
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("Nhap so luong nhan vien: ");
+            n = int.Parse(Console.ReadLine());
+            NhanVien[] list = new NhanVien[n];
+
+            //Cau1
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Nhap thong tin nhan vien thu " + (i + 1) + ":");
+                list[i] = new NhanVien();
+                list[i].input();
+            }
+            showList(list);
+            //======================================
+            //Cau2
+            Console.Write("\nNhap ten nhan vien can tim: ");
+            String name = Console.ReadLine();
+            NhanVien a = findByName(list, n, name);
+            if (a == null)
+                Console.WriteLine("\nKhong tim thay nhan vien nay");
+            else
+            {
+                Console.WriteLine("\nThong tin nhan vien can tim:");
+                a.output();
+            }
+            //Cau3
+            for (int i = 0; i < n - 1; i++)
+                for (int j = i + 1; j < n; j++)
+                    if (list[i].Luong() < list[j].Luong())
+                    {
+                        NhanVien temp;
+                        temp = list[i];
+                        list[i] = list[j];
+                        list[j] = temp;
+                    }
+
+
+            Console.WriteLine("\n***********Sau sap xep:");
+            showList(list);
+
+            Console.ReadKey();
+        }
+        static void showList(NhanVien[] list)
+        {
+            Console.WriteLine("Danh sach nhan vien:");
+            foreach (NhanVien a in list)
+                a.output();
+        }
+        static NhanVien findByName(NhanVien[] list, int n, String name)
+        {
+            for (int i = 0; i < n; i++)
+                if (string.Equals(list[i].HoTen, name))
+                    return list[i];
+            return null;
+        }
+
+    }
+}
---
+++ Day5/Test/NhanVien.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day5
+{
+    class NhanVien
+    {
+
+        //Một công ty sản xuất máy in quản lý thông tin về nhân viên bao gồm : Mã nhân viên, họ tên, hệ số lương, số ngày công, trợ cấp.
+        private String maNv;
+        private String hoTen;
+        private double heSoLuong;
+        private int soNgayCong;
+        private double troCap;
+
+        public NhanVien() { }
+
+        public string MaNv { get => maNv; set => maNv = value; }
+        public string HoTen { get => hoTen; set => hoTen = value; }
+        public double HeSoLuong { get => heSoLuong; set => heSoLuong = value; }
+        public int SoNgayCong { get => soNgayCong; set => soNgayCong = value; }
+        public double TroCap { get => troCap; set => troCap = value; }
+
+        public double Luong()
+        {
+            if (SoNgayCong >= 26)
+                return HeSoLuong * 150000 * SoNgayCong + TroCap;
+            else
+                return HeSoLuong * 150000 * SoNgayCong - ((30 - SoNgayCong) * HeSoLuong * 10000);
+        }
+        public void input()
+        {
+            Console.Write("Nhap ma nhan vien: "); MaNv = Console.ReadLine();
+            Console.Write("Nhap ten nhan vien: "); HoTen = Console.ReadLine();
+            Console.Write("Nhap he so luong: "); HeSoLuong = double.Parse(Console.ReadLine());
+            Console.Write("Nhap so ngay cong: "); SoNgayCong = int.Parse(Console.ReadLine());
+            Console.Write("Nhap tro cap: "); TroCap = double.Parse(Console.ReadLine());
+        }
+
+        public void output()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Ma nhan vien: " + MaNv);
+            Console.WriteLine("Ten nhan vien: " + HoTen);
+            Console.WriteLine("He so luong: " + HeSoLuong);
+            Console.WriteLine("So ngay cong: " + SoNgayCong);
+            Console.WriteLine("Tro cap: " + TroCap);
+            Console.WriteLine("Luong: " + this.Luong());
+            Console.WriteLine("===============================");
+        }
+    }
+}
